33df6a1d66ceb987d9e85d75bea0889d1b735127
Amina Sahovic <amina.sahovic@edu.fit.ba>
2023-08-29 22:23:53 +0200

    First commit

---
+++ eProdaja/eProdaja.Services/ProizvodiService.cs
@@ -0,0 +1,27 @@
+﻿using eProdaja.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodiService : IProizvodiService
+    {
+        List<Proizvodi> proizvodi = new List<Proizvodi>() {
+            new Proizvodi()
+            {
+                 Cijena=23,
+                JedinicaMjereId=1,
+                Sifra="112",
+                Naziv="Majica",
+                ProizvodId=1,
+            }
+        };
+        public IList<Proizvodi> Get()
+        {
+            return proizvodi;
+        }
+    }
+}
